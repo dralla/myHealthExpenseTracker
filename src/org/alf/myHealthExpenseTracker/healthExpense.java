10b17faf3cae9c86f98777bdaf241058b0bb5488
fred <fred@192.168.0.10>
2017-01-22 00:22:43 +0100

    test

--- src/org/alf/myHealthExpenseTracker/healthExpense.java
+++ src/org/alf/myHealthExpenseTracker/healthExpense.java
@@ -13,30 +13,35 @@ public class healthExpense {
 	public String getWording() {
 		return wording;
 	}
+	
 	/**
 	 * @param wording the wording to set
 	 */
 	public void setWording(String wording) {
 		this.wording = wording;
 	}
+	
 	/**
 	 * @return the medicalActdate
 	 */
 	public Date getMedicalActdate() {
 		return medicalActdate;
 	}
+	
 	/**
 	 * @param medicalActdate the medicalActdate to set
 	 */
 	public void setMedicalActdate(Date medicalActdate) {
 		this.medicalActdate = medicalActdate;
 	}
+	
 	/**
 	 * @return the bankMouvmentDate
 	 */
 	public Date getBankMouvmentDate() {
 		return bankMouvmentDate;
 	}
+	
 	/**
 	 * @param bankMouvmentDate the bankMouvmentDate to set
 	 */
