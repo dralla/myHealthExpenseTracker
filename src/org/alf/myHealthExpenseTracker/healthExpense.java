981d47bbdd27ed0d857eca6d515d62e4d090e686
fred <fred@192.168.0.10>
2017-01-22 00:20:25 +0100

    add getter

--- src/org/alf/myHealthExpenseTracker/healthExpense.java
+++ src/org/alf/myHealthExpenseTracker/healthExpense.java
@@ -7,5 +7,41 @@ public class healthExpense {
 	private String 	wording;
 	private Date 	medicalActdate;
 	private Date	bankMouvmentDate;
+	/**
+	 * @return the wording
+	 */
+	public String getWording() {
+		return wording;
+	}
+	/**
+	 * @param wording the wording to set
+	 */
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+	/**
+	 * @return the medicalActdate
+	 */
+	public Date getMedicalActdate() {
+		return medicalActdate;
+	}
+	/**
+	 * @param medicalActdate the medicalActdate to set
+	 */
+	public void setMedicalActdate(Date medicalActdate) {
+		this.medicalActdate = medicalActdate;
+	}
+	/**
+	 * @return the bankMouvmentDate
+	 */
+	public Date getBankMouvmentDate() {
+		return bankMouvmentDate;
+	}
+	/**
+	 * @param bankMouvmentDate the bankMouvmentDate to set
+	 */
+	public void setBankMouvmentDate(Date bankMouvmentDate) {
+		this.bankMouvmentDate = bankMouvmentDate;
+	}
 
 }
